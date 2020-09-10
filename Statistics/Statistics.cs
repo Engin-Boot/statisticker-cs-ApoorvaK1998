1e7381059aae273ec3d18b92d854aeb2fa6953c0
320087877 <320087877@INGBTCPIC5NBW71>
2020-09-10 18:56:35 +0530

    modi

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Statistics;
+
 
 namespace Statistics
 {
+    
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public Stats CalculateStatistics(List<float> numbers)
+        {
+
+            Stats stat=new Stats();
+            if(numbers.Count>0)
+            {
+                stat.average = numbers.Average();
+                stat.min = numbers.Min();
+                stat.max = numbers.Max();
+
+            }
+            else
+            {
+                stat.average=double.NaN;
+                stat.min = double.NaN;
+                stat.max = double.NaN;
+
+            }
+            return stat;
+
         }
     }
 }
